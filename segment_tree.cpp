8d602babd1bf20baf78910b9da1a23045db2cff2
GoGradually <chkun3109@gmail.com>
2023-09-22 15:25:59 +0900

    renew on segment tree template

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -2,36 +2,16 @@
 using namespace std;
 
 vector<int> arr;  // authorize data type of arr
-// arr[1~n]
-// root of tree = 0
+// arr = [0 : n-1]
+// root of tree = 1
 template <class T>
 class segment_tree {
    private:
     vector<T> tree;
-    int N;
 
    public:
-    segment_tree(int n) {
-        N = n - 1;
-        int h = (int)ceil(log2(n));
-        int size = 1 << (h + 1);
-        tree.resize(size);
-        build(0, 1, N);
-    }
+    segment_tree(int n) { tree.resize(4 * n); }
     T cal(T val1, T val2) { return; }  // make cal function
-    void recalculate(int node) {
-        tree[node] = cal(tree[2 * node + 1], tree[2 * node + 2]);
-    }
-    void build(int node, int left, int right) {
-        if (left == right)
-            tree[node] = arr[left];
-        else {
-            int mid = (left + right) / 2;
-            build(node * 2 + 1, left, mid);
-            build(node * 2 + 2, mid + 1, right);
-            recalculate(node);
-        }
-    }
     void update(int node, int left, int right, int x, T y) {
         if (x < left || x > right) return;
         if (left == right) {
@@ -39,18 +19,15 @@ class segment_tree {
             return;
         }
         int mid = (left + right) / 2;
-        update(node * 2 + 1, left, mid, x, y);
-        update(node * 2 + 2, mid + 1, right, x, y);
-        recalculate(node);
+        update(node * 2, left, mid, x, y);
+        update(node * 2 + 1, mid + 1, right, x, y);
+        tree[node] = cal(tree[2 * node], tree[2 * node + 1]);
     }
     T query(int node, int left, int right, int x, int y) {
         if (x > right || y < left) return null;  // designate NULL
         if (x <= left && right <= y) return tree[node];
         int mid = (left + right) / 2;
-        T lVal = query(2 * node + 1, left, mid, x, y);
-        T rVal = query(2 * node + 2, mid + 1, right, x, y);
-        return cal(lVal, rVal);
+        return cal(query(2 * node, left, mid, x, y),
+                   query(2 * node + 1, mid + 1, right, x, y));
     }
-    void update(int x, T y) { update(0, 1, N, x, y); }
-    T query(int x, int y) { return query(0, 1, N, x, y); }
 };

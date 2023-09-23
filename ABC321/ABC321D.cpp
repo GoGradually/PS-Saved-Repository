b42a9135e2201f6affb7d488a78aa1477f376ca6
GoGradually <chkun3109@gmail.com>
2023-09-23 23:09:06 +0900

    230923

---
+++ ABC321/ABC321D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+void Solve() {
+    int n, m;
+    ll p;
+    cin >> n >> m >> p;
+    vector<ll> a(n), b(m);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    vector<ll> psum(m + 1);
+    for (int i = 0; i < m; i++) {
+        psum[i + 1] = psum[i] + b[i];
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        int notP = lower_bound(b.begin(), b.end(), p - a[i]) - b.begin();
+        ans += a[i] * notP + psum[notP];
+        ans += p * (m - notP);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(문제 특징의 증명으로 아이디어){greedy, D&C, DP, graph}
+// 2. 알고리즘 "처음"부터 풀이과정 직접 전개, cutting
+// 3. cutting | 구현 | 비틀어 증명
+
+/*
+take notes.
+
+*/

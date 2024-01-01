aa6b30009ba0ac2e5af0fb191d54818fa4747548
GoGradually <62929862+GoGradually@users.noreply.github.com>
2024-01-02 00:49:56 +0900

    DP 시 종속 데이터 뭉치기

---
+++ 23_JAN/240102/13910.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(m);
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> can(10001);
+    for (int i = 0; i < m; i++) {
+        can[arr[i]] = 1;
+        for (int j = i + 1; j < m; j++) {
+            if (arr[i] + arr[j] <= n) can[arr[i] + arr[j]] = 1;
+        }
+    }
+    vector<int> dp(n + 1, INF);
+    dp[0] = 0;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * can[j]] + 1);
+        }
+    }
+    if (dp[n] >= INF)
+        cout << -1 << '\n';
+    else
+        cout << dp[n] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(문제 특징의 "증명"으로 아이디어){greedy, D&C, DP, graph, math}
+// 2. "처음"부터 직접 경우의 수 전개(수식&도식화)
+// 3. cutting | 구현(차근차근 단순화)
+
+/*
+take notes.
+
+*/
+
+// commit 시 피드백할 것 Message로 남겨두기!!

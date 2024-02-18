b256df815f958fd5315d2972c246b7c894c43623
GoGradually <chkun3109@gmail.com>
2024-02-19 00:27:00 +0900

    문제가 요구하는 알고리즘 이해, 처음부터 직접 전개하기.

---
+++ 24_FEB/240219/1931E.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n, m;
+    cin >> n >> m;
+    int ret = 0;
+    priority_queue<int> pq;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        int cnt = 0;
+        while (temp > 0 && temp % 10 == 0) {
+            temp /= 10;
+            cnt++;
+            ret++;
+        }
+        while (temp) {
+            temp /= 10;
+            ret++;
+        }
+        pq.push(cnt);
+    }
+    int turn = 1;
+    while (!pq.empty()) {
+        int now = pq.top();
+        pq.pop();
+        if (turn == 1) {
+            ret -= now;
+        }
+        turn = 1 - turn;
+    }
+    if (ret > m)
+        cout << "Sasha\n";
+    else
+        cout << "Anna\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(문제 특징의 "증명"으로 아이디어){greedy, D&C, DP, graph, math}
+// 2. "처음"부터 직접 경우의 수 전개(수식&도식화)
+// 3. cutting | 구현(차근차근 단순화)
+
+/*
+take notes.
+// 다시 보는용이 아닌
+// 현재의 흐름을 가장 잘 이어갈 수 있도록 !!!
+
+*/
+
+// commit 시 피드백할 것 Message로 남겨두기!!

e3b0f71c0d0e7cb32087945617d807ea9ffddc73
GoGradually <chkun3109@gmail.com>
2024-02-19 01:49:17 +0900

    이분탐색 연습 & 그리디 접근 연습 한가지 접근방식에 얽메이지 말고 핵심적인 관점 꿰뚫기

---
+++ 24_FEB/240219/1898B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    ll mx = arr[n - 1];
+    ll ans = 0;
+
+    for (int i = n - 2; i >= 0; i--) {
+        ll now = arr[i] / mx;
+        if (arr[i] % mx != 0) now++;
+        ll cnt = (arr[i] - 1) / mx;
+        mx = arr[i] / now;
+        ans += cnt;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(문제 특징의 "증명"으로 아이디어){greedy, D&C, DP, graph, math}
+// 2. "처음"부터 직접 경우의 수 전개(수식&도식화)
+// 3. cutting | 구현(차근차근 단순화)
+
+/*
+take notes.
+// 다시 보는용이 아닌
+// 현재의 흐름을 가장 잘 이어갈 수 있도록 !!!
+
+*/
+
+// commit 시 피드백할 것 Message로 남겨두기!!

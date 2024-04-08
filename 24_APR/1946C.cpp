e7c49bc47ca023b88a92cdb3f2642e631b5d0d8a
GoGradually <chkun3109@gmail.com>
2024-04-08 22:19:10 +0900

    vector<int> g[20000] 그래프 문제 설계 감잡기

---
+++ 24_APR/1946C.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+vector<int> g[100001];
+
+pair<int, int> dfs(int now, int before, int x) {
+    int val = 1;
+    int cnt = 0;
+    for (int i = 0; i < g[now].size(); i++) {
+        if (g[now][i] == before) continue;
+        pair<int, int> sonsAns = dfs(g[now][i], now, x);
+        val += sonsAns.first;
+        cnt += sonsAns.second;
+    }
+    if (val > x) {
+        val = 0;
+        cnt++;
+    }
+    return make_pair(val, cnt);
+}
+
+void Solve() {
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i <= n; i++) {
+        g[i].clear();
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+
+    int left = 1, right = INF;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        int cnt, val;
+        tie(val, cnt) = dfs(1, 0, mid);
+        if (cnt > k) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    cout << left << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+1. int 오버플로우, out of bounds
+2. 특수한 경우(n=1?)
+3. 잘못된 가정 & 증명
+
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기, 구하고자 하는 것의 계층구조 표현하기.
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(문제 특징의 "증명"으로 아이디어)
+// {BruteForce, greedy, D&C, DP, graph, math}
+// 2. "처음"부터 직접 경우의 수 전개(수식&도식화)
+// 3. cutting | 구현(차근차근 단순화 & 최적화)
+
+/*
+take notes.
+// 다시 보는용이 아닌
+// 현재의 흐름을 가장 잘 이어갈 수 있도록 !!!
+
+*/
+
+// commit 시 피드백할 것 Message로 남겨두기!!
+// 틀리면 느낌표 추가하기

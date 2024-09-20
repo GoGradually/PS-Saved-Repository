49f53790383dc6de98bfc7299a3c07e76bc4615f
GoGradually <chkun3109@gmail.com>
2024-09-20 15:29:51 +0900

    가장 단위 동작의 동작 방식(조건분기) 파악

---
+++ 24_SEP/240920/31963.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF ((int)1e9 + 10)
+#define lINF ((ll)1e18 + 10LL)
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    vector<ll> cnt(n, 0);
+    for (int i = 1; i < n; i++) {
+        cnt[i] = cnt[i - 1];
+        int before = arr[i - 1];
+        int now = arr[i];
+        if (before > now) {
+            while (before > now) {
+                now <<= 1;
+                cnt[i]++;
+            }
+        } else {
+            while (before < now && cnt[i] > 0) {
+                before <<= 1;
+                cnt[i]--;
+            }
+            if (before > now) {
+                cnt[i]++;
+            }
+        }
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += cnt[i];
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+1. int 오버플로우, out of bounds
+2. 특수한 경우(n=1?)
+3. 잘못된 가정 & 증명
+
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기 & 구하고자 하는 것의 계층 구조 표현 -> 정확히 & 입체적으로 파악
+*한가지 접근 방식에 얽메이지 말기 -> 응용의 끝 알기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. 아이디어 획득 : "추론"
+//     - 문제 알고리즘/특징의 "증명"으로 아이디어
+//         - {BruteForce, greedy, D&C, DP, graph, math}-
+//     - “책임” 뽑아내기
+//         - 각 기능들이 어떤 책임을 가지고 있는지
+//         - “어떤 패턴”으로 동작하는지
+//     - 가장 Naive한 상태/알고리즘부터 시작하기 (완전 탐색, 단순 자료구조)
+//     - 가장 극단적인 경우에서 추론 (가장 차이가 뚜렷한 예제)
+// 2. "처음"부터 직접 경우의 수 전개(수식&도식화, 엄밀한 조건 정리)
+//     - 알고리즘 내에 어떤 역할들이 있는지 -> 직접 들어가보기
+//     - 알고리즘 내에 어떤 기능들이 있는지 -> 직접 수행해보기
+// 3. cutting | 구현(차근차근 단순화 & 최적화)
+
+/*
+take notes.
+// 다시 보는용이 아닌
+// 현재의 흐름을 가장 잘 이어갈 수 있도록 !!!
+
+*/
+
+// commit 시 피드백할 것 Message로 남겨두기!!
+// 틀리면 느낌표 추가하기
